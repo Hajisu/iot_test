b1d2c234b4f31f25711d96c4ca06439cb80a1582
Hajisu <govlwltn625@naver.com>
2017-01-24 12:52:53 +0900

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,15 @@
+package Test01;
+
+public class sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		int num;
+		for(int i=1; i<=100; i++){
+			sum+=i;
+		}
+		System.out.println("합계 : "+sum);
+	}
+
+}
